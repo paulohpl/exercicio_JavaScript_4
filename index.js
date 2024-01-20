0df5a836448601980efdc617f744dbf742611cfa
Paulo <paulo.ipa9@gmail.com>
2024-01-20 13:41:18 -0300

    Algumas atividades JS do curso da Alura

---
+++ index.js
@@ -0,0 +1,34 @@
+// 1 Crie uma lista vazia, com o nome listaGenerica
+
+let listaGenerica = [];
+
+// 2 Crie uma lista de linguagens de programação chamada 
+// linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+
+let linguagensDeProgramacao = ['javascript', 'C', 'C++', 'Kotlin', 'Python'];
+
+// 3 Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+
+let linguagensDeProgramacao = ['javascript', 'C', 'C++', 'Kotlin', 'Python'];
+
+linguagensDeProgramacao.push ('java','Ryby','GoLang'); */
+
+// 4 Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento. 
+
+let listaComTresNomes = ['Cleidson', 'Dormeval', 'Janiclistie'];
+
+console.log(listaComTresNomes[0]);
+
+// 5 Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+
+let listaComTresNomes = ['Cleidson', 'Dormeval', 'Janiclistie'];
+
+console.log(listaComTresNomes[1]);
+
+// 6 Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+
+
+let listaComTresNomes = ['Cleidson', 'Dormeval', 'Janiclistie'];
+
+console.log(listaComTresNomes[2]);
+
